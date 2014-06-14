676ee8dca0857e8feb8ae16c25d9445cc51f5a6f
Rune Halvorsen <runefh@gmail.com>
2014-06-15 01:37:42 +0200

    Added mongoose

--- web.js
+++ web.js
@@ -1,15 +1,25 @@
-// web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log('yay')
+});
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
