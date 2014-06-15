57c9058088e3ad196d16b9abb1c69398915c5c05
Rune Halvorsen <runefh@gmail.com>
2014-06-15 20:22:21 +0200

    Added stuff for new view

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var logfmt = require('logfmt');
 var pmongo = require('promised-mongo');
 var swig = require('swig');
 var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
-
+var validator = require('express-validator');
+var uuid = require('uuid');
 
 var app = express();
 
@@ -17,13 +18,15 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
 swig.setDefaults({ cache: false });
 
+app.use(bodyParser());
+app.use(validator());
+
 app.use(session({
     name: 'reorgbingo',
-    secret: "opirf3fporf-3f-kfmcm43f-k[o2pmqcqpof-3o4fqpmrwbwnibnbwp34043pogrgpoe04",
+    secret: 'opirf3fporf-3f-kfmcm43f-k[o2pmqcqpof-3o4fqpmrwbwnibnbwp34043pogrgpoe04',
 }));
 
-
-var connectionString = "morradi"
+var connectionString = 'morradi'
 
 var db = pmongo(connectionString, ['games', 'guesses']);
 
@@ -41,10 +44,40 @@ app.get('/new', function(req, res) {
 });
 
 app.post('/new', function(req, res) {
-    res.send('Hello World!, new games');
+    req.checkBody('email').notEmpty().isEmail();
+    req.checkBody('title').notEmpty();
+
+    var errors = req.validationErrors(true)
+
+    if (errors) {
+        res.render('new', {
+            form: req.body,
+            errors: req.validationErrors(true)
+        });        
+    }
+    else {
+        games.insert({
+            title: req.body.title,
+            description: req.body.body,
+            ownerEmail: req.body.email,
+            id: uuid.v4(),
+            publicId: uuid.v4(),
+            ownerId: uuid.v4()
+        }).then(console.log).done();
+    }    
 });
+
+app.get('/g/:id', function(req, res) {
+    games.findOne({id: req.params.id}).then(function(game) {
+        console.log(game);
+        res.render('game', {
+            game: game
+        });
+    });
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
